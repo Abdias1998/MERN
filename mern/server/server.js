f031b69cc1136820e85200e7f63d82c64d2a4667
Abdias1998 <lagraceparle98@gmail.com>
2025-09-26 16:06:35 +0100

    First commit

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,13 +1,49 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import rate_limiter from "express-rate-limit";
+import bodyParser from "body-parser";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 5050;
-const app = express();
 
-app.use(cors());
+const app = express();
+const origineClient = "http://localhost:5050";
+
+app.use(cookieParser());
+// app.use(helmet());
+
+// ⚠️ express-rate-limit doit être configuré comme middleware
+const limiter = rate_limiter({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // 100 requêtes par IP
+});
+app.use(limiter);
+
+app.use(cors({ credentials: true, origin: origineClient }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use("/record", records);
+
+// 👉 Sert le frontend buildé (React, par ex.)
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+app.get("/create", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 // start the Express server
 app.listen(PORT, () => {
